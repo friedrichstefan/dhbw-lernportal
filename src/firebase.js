8ee47f8f6c6de5f0bd993f6e0da8ab47fe762cde
I767513 <stefan.friedrich01@sap.com>
2026-07-01 22:00:10 +0200

    feat: add Firebase SDK and initialize app

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJkhA05noZCoeavSND79wDO6ERk-FVYHg",
+  authDomain: "dhbw-2semester.firebaseapp.com",
+  projectId: "dhbw-2semester",
+  storageBucket: "dhbw-2semester.firebasestorage.app",
+  messagingSenderId: "345438968240",
+  appId: "1:345438968240:web:4da2eb821c54bb3a9a9e75"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
